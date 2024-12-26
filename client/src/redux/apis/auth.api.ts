e3d8e41b564eaaf9d5be53276a42905b69b59254
Ehte22 <syedehteshamali22@gmail.com>
2024-12-26 12:45:04 +0530

    domain update again

--- client/src/redux/apis/auth.api.ts
+++ client/src/redux/apis/auth.api.ts
@@ -3,7 +3,7 @@ import { IUser } from "../../models/user.interface"
 
 export const authApi = createApi({
     reducerPath: "authApi",
-    baseQuery: fetchBaseQuery({ baseUrl: `${process.env.BASE_URL}/api/v1/auth`, credentials: "include" }),
+    baseQuery: fetchBaseQuery({ baseUrl: `https://auth-app-one-nu.vercel.app/api/v1/auth`, credentials: "include" }),
     tagTypes: ["auth"],
     endpoints: (builder) => {
         return {
